32207ca3db4ee7923b8ad8979ec3dfc8c892cfbe
Jaqueline Andrade <jaqueline8118@gmail.com>
2025-04-15 18:12:30 -0300

    Correção Builder

--- src/framework/builder/BuilderJogador.java
+++ src/framework/builder/BuilderJogador.java
@@ -3,8 +3,9 @@ package builder;
 import model.Jogador;
 
 public interface BuilderJogador {
-    BuilderJogador comNome(String nome);
-    BuilderJogador comCor(String cor);
-    BuilderJogador comPontos(int pontos);
+    void configurarNome(String nome);
+    void configurarCor(String cor);
+    void configurarPontos(int pontos);
     Jogador construir();
+
 }
--- src/framework/builder/DiretorJogador.java
+++ src/framework/builder/DiretorJogador.java
@@ -4,11 +4,16 @@ import model.Jogador;
 
 public class DiretorJogador {
 
-    public Jogador criarNovo(String nome, String cor) {
-        return new ConstrutorJogador()
-            .comNome(nome)
-            .comCor(cor)
-            .comPontos(0)
-            .construir();
+    private final ConstrutorJogador construtor;
+
+    public DiretorJogador(ConstrutorJogador construtor) {
+        this.construtor = construtor;
+    }
+
+    public Jogador criarJogador(String nome, String cor, int pontos) {
+        construtor.configurarNome(nome);
+        construtor.configurarCor(cor);
+        construtor.configurarPontos(0);
+        return construtor.construir();
     }
 }
